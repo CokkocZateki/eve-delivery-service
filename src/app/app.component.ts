386b705f38e500f3a03dd18844ec322c57181b5e
michaelbahr <mbahr92@gmail.com>
2016-07-30 17:54:49 +0200

    added alive check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ErrorComponent} from "./errors/error.component";
 import {OrderComponent} from "./order/order.component";
 import {StatusComponent} from "./status/status.component";
 import {FeedbackComponent} from "./feedback/feedback.component";
+import {Http} from "@angular/http";
+import {environment} from "./environment";
 
 @Component({
   moduleId: module.id,
@@ -11,13 +13,20 @@ import {FeedbackComponent} from "./feedback/feedback.component";
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Horde Supermarket";
 
   public serviceDown = false;
 
+  public constructor (private http:Http) {}
+
   ngOnInit() {
-    // todo: load dynamically
-    this.serviceDown = false;
+    this.http.get(environment.ip + "v1/alive/alive").subscribe(
+      data => this.serviceDown = false,
+      err => {
+        this.serviceDown = true;
+        console.log(err);
+      }
+    );
   }
 }
