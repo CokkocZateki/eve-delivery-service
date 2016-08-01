06685ec77ce546396df4da88db7d49f375ea090f
michaelbahr <mbahr92@gmail.com>
2016-08-01 09:20:13 +0200

    port update

--- runWeb.sh
+++ runWeb.sh
@@ -1,4 +1,4 @@
 rm -Rf dist
 
 pidof angular-cli | xargs kill -9
-nohup ng serve --port 80 -prod > web.out 2> web.err < /dev/null &
+nohup ng serve --port 8080 -prod > web.out 2> web.err < /dev/null &
