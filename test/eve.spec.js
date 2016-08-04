6a8e12033f56f422b091d7aab8208983a6a078e7
michaelbahr <mbahr92@gmail.com>
2016-08-04 16:23:12 +0200

    client ui tests

---
+++ test/eve.spec.js
@@ -0,0 +1,63 @@
+// spec.js
+describe('Protractor Demo App', function () {
+  var name = element(by.id('characterName'));
+  var link = element(by.id('evepraisalLink'));
+  var submitLink = element(by.id('submitLink'));
+  var quoteResult = element(by.id('quoteResult'));
+  var submitOrder = element(by.id('submitOrder'));
+  var orderSubmittedMessage = element(by.id('orderSubmittedMessage'));
+  var showStatusButton = element(by.id('showStatusButton'));
+  var statusDiv = element(by.id('status'));
+  var showFeedbackButton = element(by.id('showFeedbackForm'));
+  var feedbackInput = element(by.id('feedbackInput'));
+  var feedbackSubmittedMessage = element(by.id('feedbackSubmittedMessage'));
+  var submitFeedback = element(by.id('submitFeedback'));
+
+  var EC = protractor.ExpectedConditions;
+
+  beforeEach(function () {
+    browser.get('http://localhost:4200');
+  });
+
+  it('should have a title', function () {
+    expect(browser.getTitle()).toEqual('Horde Delivery Service');
+  });
+
+  it('should get quote and submitting should show result page', function () {
+    name.sendKeys('Test');
+    link.sendKeys('http://evepraisal.com/e/11856686');
+
+    submitLink.click();
+
+    browser.wait(EC.presenceOf(quoteResult), 10000);
+    expect(quoteResult.isPresent()).toBeTruthy();
+    expect(quoteResult.getText()).toContain(".00 ISK");
+
+    submitOrder.click();
+
+    browser.wait(EC.presenceOf(orderSubmittedMessage), 3000);
+
+    expect(orderSubmittedMessage.isPresent()).toBeTruthy();
+  });
+
+  it('should show status box', function () {
+    showStatusButton.click();
+
+    browser.wait(EC.presenceOf(statusDiv), 500);
+    expect(statusDiv.isPresent()).toBeTruthy();
+  });
+
+  it('should show feedback box and be able to send feedback', function () {
+    showFeedbackButton.click();
+
+    browser.wait(EC.presenceOf(feedbackInput), 500);
+    expect(feedbackInput.isPresent()).toBeTruthy();
+
+    feedbackInput.sendKeys('Test');
+    submitFeedback.click();
+
+    browser.wait(EC.presenceOf(feedbackSubmittedMessage), 1000);
+    expect(feedbackSubmittedMessage.isPresent()).toBeTruthy();
+  });
+
+});
