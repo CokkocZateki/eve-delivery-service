a15ff32010465b90198924fe3410e7659c7bacb4
michaelbahr <mbahr92@gmail.com>
2016-08-07 02:17:33 +0200

    updated dev-base url and test config

--- config/environment.dev.ts
+++ config/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  ip: "http://localhost:4000/"
+  ip: "http://127.0.0.1:8080/"
 };
--- test/conf.js
+++ test/conf.js
@@ -2,6 +2,14 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    // 'browserName': 'internet explorer', - special installation needed
+    // 'version':'10',
+    'browserName': 'chrome',
+    //'browserName': 'firefox'
+  },
+  baseUrl:'http://127.0.0.1:8080',
   specs: ['client.spec.js'],
   useAllAngular2AppRoots: true
 }
