9e06420507ead3fb69d8c93369d726d9bd062c48
michaelbahr <mbahr92@gmail.com>
2016-09-10 18:21:54 +0200

    fixed timestamp

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {MarketService} from "../services/market.service";
 import {MarketItem} from "../common/marketItem";
+import {LocalDateTimeToDate} from "../common/localDateTimeToDate";
 
 @Component({
   moduleId: module.id,
   selector: 'market',
   templateUrl: 'market.component.html',
   styleUrls: ['market.component.css'],
-  providers: [MarketService]
+  providers: [MarketService],
+  pipes: [LocalDateTimeToDate]
 })
 export class MarketComponent implements OnInit {
 
