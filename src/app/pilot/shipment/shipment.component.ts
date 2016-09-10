d82f4a85b3dd77f16143c5837bd042b0e9a86aa1
michaelbahr <mbahr92@gmail.com>
2016-09-10 16:01:03 +0200

    added multi complete buttons

--- src/app/pilot/shipment/shipment.component.ts
+++ src/app/pilot/shipment/shipment.component.ts
@@ -6,18 +6,21 @@ import {PilotService} from "../../services/pilot.service";
 import {Order} from "../../common/order";
 import {ManagerService} from "../../services/manager.service";
 import {NumberGrouping} from "../../common/numberGrouping.pipe";
+import {ClipboardDirective} from "angular2-clipboard";
 
 @Component({
   selector: 'shipment',
   templateUrl: './app/pilot/shipment/shipment.component.html',
   providers: [provide(AuthConfig, {useValue: new AuthConfig()}), AuthHttp, PilotService, OrderProcessingService,
     ManagerService],
-  directives: [ContractedDialogComponent],
-  pipes: [NumberGrouping]
+  directives: [ContractedDialogComponent, ClipboardDirective],
+  pipes: [NumberGrouping],
 })
 export class ShipmentComponent implements OnInit {
 
   public orders:Array<any> = undefined;
+
+  allClients:string = "";
 
   constructor(private service:PilotService, private orderProcessing:OrderProcessingService) {
   }
@@ -26,12 +29,28 @@ export class ShipmentComponent implements OnInit {
     this.service.listShippingOrders().subscribe(
       data => {
         this.orders = data.json();
+        this.setAllClients();
       },
       err => {
         alert(err);
         console.log(err);
       }
     );
+  }
+
+  setAllClients() {
+    for (var i = 0; i < this.orders.length; i++) {
+      let order = this.orders[i];
+      this.allClients += order.client + ", ";
+    }
+  }
+
+  contractedAll() {
+    for (var i = 0; i < this.orders.length; i++) {
+      let order = this.orders[i];
+      this.orders = this.orderProcessing.onStatusChange(order.id, 'contracted', this.orders);
+      order.status = 'contracted';
+    }
   }
 
 
@@ -47,17 +66,10 @@ export class ShipmentComponent implements OnInit {
 
   onStatusChange(orderId:string, status:string) {
     this.orders = this.orderProcessing.onStatusChange(orderId, status, this.orders);
-    for(var i = 0; i < this.orders.length; i++) {
+    for (var i = 0; i < this.orders.length; i++) {
       if (orderId === this.orders[i].id) {
         this.orders[i].status = 'contracted';
       }
     }
   }
-
-  getCompleteMessage() {
-    var compliments = ['You\'re awesome!', 'Keep up the good work!', 'Keep on rollin\'', 'JF pilot of our hearts!'];
-    var result = compliments[Math.floor(Math.random() * compliments.length)];
-    return result;
-  }
-
 }
