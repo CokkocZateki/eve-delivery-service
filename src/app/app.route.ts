22bfd852b066795d022a464572c9f2948fa330cd
michaelbahr <mbahr92@gmail.com>
2016-10-02 18:36:43 +0200

    removed broken route

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -4,7 +4,6 @@ import {ManagerComponent} from "./manager/manager.component";
 import {PilotComponent} from "./pilot/pilot.component";
 import {ManagerDetailComponent} from "./manager/manager-detail/manager-detail.component";
 import {MarketComponent} from "./market/market.component";
-import {LoginComponent} from "./login/login.component";
 import {RaffleComponent} from "./raffle/raffle.component";
 
 const routes: RouterConfig = [
@@ -12,7 +11,6 @@ const routes: RouterConfig = [
   { path: 'manager/:id', component: ManagerDetailComponent },
   { path: 'pilot', component: PilotComponent },
   { path: 'market', component: MarketComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'raffle', component: RaffleComponent },
   { path: '**', component: ClientComponent }
 ];
