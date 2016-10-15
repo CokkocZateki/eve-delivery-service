3c6949298f0434dac6a0620e94038f02b9d2bd5d
michaelbahr <mbahr92@gmail.com>
2016-10-15 11:23:19 +0200

    trying to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 echo $PRIVATE_SSH_KEY >> /home/.ssh/id_rsa
 chmod 0600 /home/.ssh/id_rsa
 echo $KNOWN_HOSTS >> /home/.ssh/known_hosts
-#chmod 0644 /home/.ssh/authorized_keys
-ssh -o StrictHostKeyChecking=no frontend@139.59.178.23 'time'
+chmod 0644 /home/.ssh/known_hosts
+ssh -o StrictHostKeyChecking=no -i /home/.ssh/id_rsa frontend@139.59.178.23 'time'
 
 #cat /etc/ssh/ssh_config
 
