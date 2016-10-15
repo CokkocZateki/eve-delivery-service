dc4cfe5305ac45ca451f842f6e71bdf0b3943fc0
michaelbahr <mbahr92@gmail.com>
2016-10-15 11:19:24 +0200

    trying to deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo $PRIVATE_SSH_KEY >> /home/.ssh/id_rsa
+chmod 0600 /home/.ssh/id_rsa
+echo $KNOWN_HOSTS >> /home/.ssh/known_hosts
+#chmod 0644 /home/.ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no frontend@139.59.178.23 'time'
+
+#cat /etc/ssh/ssh_config
+
+
+# /home/.ssh/authorized_keys
+# -o StrictHostKeyChecking=no -o LogLevel=error
