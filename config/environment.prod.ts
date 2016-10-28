29c52588a00e5fc4269922cde7b76e2da4eca31a
michaelbahr <mbahr92@gmail.com>
2016-10-28 23:40:13 +0200

    added missing slash

--- config/environment.prod.ts
+++ config/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   ip: "https://hordedelivery.com:4001/",
   ipV2: "https://hordedelivery.com:8444/",
-  ipAuth: "https://hordedelivery.com:8443",
+  ipAuth: "https://hordedelivery.com:8443/",
   ssoUrl: "https://login.eveonline.com/oauth/authorize/?response_type=code&redirect_uri=http%3A%2F%2Fhordedelivery.com%2Fcallback&client_id=7f976b797f6b47009a6eb62d115b5066&scope=publicData"
 };
