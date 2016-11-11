e27712e592efe8f41e80b8715aee76c42c089c6c
michaelbahr <mbahr92@gmail.com>
2016-11-11 15:29:39 +0100

    wip

--- src/app/pilot/self-service/self-service.component.ts
+++ src/app/pilot/self-service/self-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PilotService} from "../../services/pilot.service";
 
 @Component({
   moduleId: module.id,
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelfServiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pilotService:PilotService) { }
 
   ngOnInit() {
+    // this.pilotService.getRequestedOrders()
   }
+
+
 
 }
