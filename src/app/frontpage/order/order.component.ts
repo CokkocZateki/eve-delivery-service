26a4f5f04e8a135025dc196ef9622587b120da53
michaelbahr <mbahr92@gmail.com>
2016-11-13 16:17:03 +0100

    readded multi-order; removed "buy again"

--- src/app/frontpage/order/order.component.ts
+++ src/app/frontpage/order/order.component.ts
@@ -16,8 +16,6 @@ import {Router} from "@angular/router";
 export class OrderComponent {
   private expectedPrice: number;
   private isPriceCalculationFailed = false;
-  // this is a workaround-ish to enable submit when we show the order page again for a certain order
-  isReorder: boolean = false;
 
   public constructor(private orderService: OrderService, private router: Router) {
 
@@ -53,8 +51,6 @@ export class OrderComponent {
         // todo: show frontend error message
       }
     );
-
-    this.isReorder = false;
   }
 
   public askQuote(link: string) {
@@ -97,13 +93,8 @@ export class OrderComponent {
     this.submitted = false;
     this.expectedPrice = null;
     this.isPriceCalculated = false;
+    this.multiplier = 1;
     this.router.navigate(['/']);
-  }
-
-  showOrderPageUntouched() {
-    this.submitted = false;
-    this.router.navigate(['/']);
-    this.isReorder = true;
   }
 
   multiplierChanged() {
