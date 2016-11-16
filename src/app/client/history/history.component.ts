fc955f7d6e1889132d53cd0ad14edb602b3c3580
michaelbahr <mbahr92@gmail.com>
2016-11-16 18:23:50 +0100

    added client-service history and load it in history component

--- src/app/client/history/history.component.ts
+++ src/app/client/history/history.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {Order} from "../../common/order";
+import {ClientService} from "../../services/client.service";
 
 @Component({
   moduleId: module.id,
   selector: 'app-history',
   templateUrl: 'history.component.html',
-  styleUrls: ['history.component.css']
+  styleUrls: ['history.component.css'],
+  providers: [ClientService]
 })
 export class HistoryComponent implements OnInit {
 
-  private orders: Order[];
+  orders: Order[];
 
-  constructor() { }
+  constructor(private service:ClientService) { }
 
   ngOnInit() {
+    this.service.history().then(orders => this.orders = orders);
   }
 
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Http, Headers} from "@angular/http";
 import {environment} from "../environment";
+import {Order} from "../common/order";
 
 @Injectable()
 export class ClientService {
@@ -27,5 +28,17 @@ export class ClientService {
 
   delete(orderId: string) {
     return this.http.post(this.baseUrl + "delete?id=" + orderId, "", this.auth());
+  }
+
+  history(): Promise<Order[]> {
+    return this.http.get(this.baseUrl + "history", this.auth())
+      .toPromise()
+      .then(response => response.json() as Order[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 }
