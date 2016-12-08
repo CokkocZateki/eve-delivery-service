ded2e92e334cad70699b13bebbcf4885efa00fdf
michaelbahr <mbahr92@gmail.com>
2016-12-08 17:07:33 +0100

    added info-fitting component

---
+++ src/app/pilot/shipment/info-fitting/info-fitting.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-info-fitting',
+  templateUrl: 'info-fitting.component.html',
+  styleUrls: ['info-fitting.component.css']
+})
+export class InfoFittingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
