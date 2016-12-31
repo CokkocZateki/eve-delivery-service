010da95e74b71d997cc7799ae2b1839e145db28d
bahrmichael <mbahr92@gmail.com>
2016-12-31 19:15:48 +0100

    added service + uuid generator

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {SurveyService} from "../services/survey.service";
 
 @Component({
   moduleId: module.id,
   selector: 'app-survey',
   templateUrl: 'survey.component.html',
-  styleUrls: ['survey.component.css']
+  styleUrls: ['survey.component.css'],
+  providers: [SurveyService]
 })
 export class SurveyComponent implements OnInit {
 
@@ -19,5 +21,18 @@ export class SurveyComponent implements OnInit {
   answerWith(answer:string): void {
     console.log(answer);
     this.answered = true;
+  }
+
+  generateUUID() {
+    let d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === "function") {
+      d += performance.now(); //use high-precision timer if available
+    }
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
   }
 }
