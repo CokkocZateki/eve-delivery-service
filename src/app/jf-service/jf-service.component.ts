17f78b197aeea807dc445a4182b0927fac849880
bahrmichael <mbahr92@gmail.com>
2017-01-01 19:54:17 +0100

    init

---
+++ src/app/jf-service/jf-service.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-jf-service',
+  templateUrl: 'jf-service.component.html',
+  styleUrls: ['jf-service.component.css']
+})
+export class JfServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
